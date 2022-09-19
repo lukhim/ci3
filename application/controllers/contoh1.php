0d80f193162fe5bb58eae768843e6efdba3be153
lukhim <lukmanhak02@gmail.com>
2022-09-19 16:42:23 +0700

    first commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Let Me Introduce .... myself</h1>";
+        echo "Nama Saya Lukmanul Hakim. Saya tingal di daerah CIkarang Baru. Olahraga yang saya sukai adalah Basket";
+    }
+}
