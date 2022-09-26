1e0d5ea8de6265422683f8db198e5ad046b065f3
lukhim <lukmanhak02@gmail.com>
2022-09-26 15:51:07 +0700

    commit4 connit4

---
+++ application/models/Model_latihan1.php
@@ -0,0 +1,13 @@
+<?php class Model_latihan1 extends CI_Model 
+{ 
+    //membuat variable untuk menampung nilai 
+    public $nilai1, $nilai2, $hasil;
+    //method penjumlahan 
+    public function jumlah($n1 = null, $n2 = null) 
+    { 
+        $this->nilai1 = $nil1; 
+        $this->nilai2 = $nil2; 
+        $this->hasil = $this->nilai1 + $this->nilai2; 
+        return $this->hasil; 
+    } 
+}
---
+++ application/views/view_latihan.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>latihan1</title>
+</head>
+<body>
+    Hallo kawan.. yk kita belaja web progreamming..!!!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    Ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = ". $hasil; ?>
+
+</body>
+</html>
